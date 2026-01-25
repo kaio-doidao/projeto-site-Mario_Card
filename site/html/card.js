e5dd018932279f073f627a57b41d514d55329c4e
SofoclesFeitosa <anaxagorassofocles@gmail.com>
2026-01-25 17:42:44 -0300

    terminei

---
+++ site/html/card.js
@@ -0,0 +1,10 @@
+const menuDiv = document.getElementById('links-mobile')
+const btnAnimar = document.getElementById('btn-menu')
+
+menuDiv.addEventListener('click', animar)
+
+function animar(){
+    menuDiv.classList.toggle('abrir')
+    btnAnimar.classList.toggle('ativar')
+}                
+           
